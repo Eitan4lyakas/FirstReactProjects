aa69f88c6359477795d8ad03e0eec9f3eca4b048
Eitan4lyakas <eitan.lyakas@gmail.com>
2024-08-11 22:41:49 +0300

    added the list of the finished tasks
    only need a style

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,9 +3,9 @@ import React, {useState} from "react";
 function ToDoList(){
 
     const [taskList, setTaskList] = useState([]);
+    const [finishedTasksList, setFinishedTasksList] = useState([]);
     const [taskName, setTaskName] = useState("");
     const [taskDueDate, setTaskDueDate] = useState(new Date().toDateString());
-    const [taskStatus, setTaskStatus] = useState(false); //false: not finished true: finished
 
     const taskDate = new Date().toDateString();
 
@@ -14,7 +14,6 @@ function ToDoList(){
             name: taskName,
             creationDate: taskDate,
             dueDate: taskDueDate,
-            status: taskStatus
         };
 
         setTaskList(prevTasks =>[...prevTasks, newTask]);
@@ -26,7 +25,17 @@ function ToDoList(){
     }
 
     function handleRemoveTask(index){
+        //console.log(taskList[index]);
+        const finishedTask = {
+            name: taskList[index].name,
+            creationDate: taskList[index].creationDate,
+            dueDate: taskList[index].dueDate
+        }
 
+        setFinishedTasksList(prevTasks =>[...prevTasks, finishedTask]);
+        setTaskList(prevTasks => prevTasks.filter((_, i) => i !== index));
+
+        
     }
 
     function handleNameChange(event){
@@ -37,36 +46,43 @@ function ToDoList(){
         setTaskDueDate(event.target.value);
     }
 
-    function handleStatusChange(index){
-        const updatedTaskList = taskList.map((task, i) => {
-            if (i === index) {
-                return { ...task, status: !task.status };
-            }
-            return task;
-        });
-
-        setTaskList(updatedTaskList);
-    }
-
     return (
         <div className="ToDo-list-container">
             <h1 className="title">Your TO-DO list</h1>
+            <div className="small-titles">
+                <h2>Date</h2> <h2>Task</h2> <h2>Due Date</h2>
+            </div>
             <ul className="list">
                 {taskList.map((task, index) => (
                     <li key={index}>
-                        <input 
+                        <p className="details">{task.creationDate}</p>  
+                            <p className="details">{task.name}</p>  
+                            <p className="details">{task.dueDate}</p>
+                        <input
                             type="checkbox" 
-                            checked={task.status} 
-                            onChange={() => handleStatusChange(index)} 
+                            onChange={() => handleRemoveTask(index)} 
                         />
-                        {task.creationDate} - {task.name} - {task.dueDate}
                     </li>
                 ))}
             </ul>
-
-            <input type="text" placeholder="Task Name" value={taskName} onChange={handleNameChange} />
-            <input type="date" value={taskDueDate} onChange={handleDueDateChange} />
-            <button onClick={handleAddTask}>Add task</button>
+            
+            <div className="inputButtons">
+                <input type="text" placeholder="Task Name" value={taskName} onChange={handleNameChange} />
+                <input type="date" value={taskDueDate} onChange={handleDueDateChange} /> <br></br>
+                <button onClick={handleAddTask}>Add task</button>
+            </div>
+            <div className="finished-tasks">
+                <h1>Finished tasks:</h1> 
+                <ul className="Finished-list">
+                    {finishedTasksList.map((finishedTask, index) => (
+                        <li key={index}>
+                            <p className="details">{finishedTask.creationDate}</p>  
+                                <p className="details">{finishedTask.name}</p>  
+                                <p className="details">{finishedTask.dueDate}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
